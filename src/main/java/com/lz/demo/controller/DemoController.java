1bfc67907d7f1371369016b822dde4afeff952a7
lizhenhua@beyondcent.com <6639020h>
2020-12-28 18:30:20 +0800

    删除test

--- src/main/java/com/lz/demo/controller/DemoController.java
+++ src/main/java/com/lz/demo/controller/DemoController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("demo")
+
 @RestController
 public class DemoController {
     @GetMapping("hello")

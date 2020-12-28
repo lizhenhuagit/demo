978cb7d5b1bdb99722cc73c219adcf285cde7146
lizhenhua@beyondcent.com <6639020h>
2020-12-28 17:18:35 +0800

    新分支

--- src/main/java/com/lz/demo/controller/DemoController.java
+++ src/main/java/com/lz/demo/controller/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("demo")
+@RequestMapping("v1.0")
 @RestController
 public class DemoController {
     @GetMapping("hello")

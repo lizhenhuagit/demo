4836184545c07534cac2d1bd92d102a5addf5749
lizhenhua@beyondcent.com <6639020h>
2020-12-29 10:38:16 +0800

    删除test

--- src/main/java/com/lz/demo/controller/DemoController.java
+++ src/main/java/com/lz/demo/controller/DemoController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("hello")
     public String ss(){
-        return "hello world!";
+        return "早上好啊，臻臻!";
     }
 }
